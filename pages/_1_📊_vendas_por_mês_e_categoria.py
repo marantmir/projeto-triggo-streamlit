c33a735c770d9cbe69ddbd7a17e7319602744d62
Marco Antonio Miranda <marcoa.miranda@usp.br>
2025-05-18 13:30:55 -0300

    Update _1_📊_vendas_por_mês_e_categoria.py

--- pages/_1_📊_vendas_por_mês_e_categoria.py
+++ pages/_1_📊_vendas_por_mês_e_categoria.py
@@ -3,7 +3,9 @@ import pandas as pd
 
 @st.cache_data
 def get_monthly_data():
-    df = st.session_state.df.copy()
+    from utils.load_data import download_and_cache_data
+    df = download_and_cache_data()
+    df['order_purchase_timestamp'] = pd.to_datetime(df['order_purchase_timestamp'])
     df['purchase_month'] = df['order_purchase_timestamp'].dt.to_period('M').astype(str)
     monthly_orders = df.groupby('purchase_month').size().reset_index(name='order_count')
     return monthly_orders
